8dc91523685e51450bc92615a3a6c0c540843cf1
zic9494 <98081424+zic9494@users.noreply.github.com>
2026-07-06 18:39:10 +0800

    Routing 註冊

--- src/quickmlops/routing.py
+++ src/quickmlops/routing.py
@@ -2,15 +2,22 @@ from typing import Annotated, Any, TypeVar, Awaitable
 from annotated_doc import Doc
 from typing_extensions import deprecated
 
+from collections.abc import Callable, Coroutine, Sequence
+
 from starlette import routing
 from starlette.routing import BaseRoute
 from starlette.types import ASGIApp
 
-class APIRoute(routing.Route):
-    def __init__(self, path, endpoint, *, methods = None, name = None, include_in_schema = True, middleware = None):
-        super().__init__(path, endpoint, methods=methods, name=name, include_in_schema=include_in_schema, middleware=middleware)
-        pass
+DecoratedCallable = TypeVar("DecoratedCallable", bound=Callable[..., Any])
 
+class APIRoute(routing.Route):
+    def __init__(self, path, endpoint, methods=None, name=None):
+        super().__init__(
+            path=path,
+            endpoint=endpoint,
+            methods=methods,
+            name=name
+        )
 
 class APIRouter(routing.Router):
     def __init__(
@@ -40,7 +47,134 @@ class APIRouter(routing.Router):
 
         #other
         self.deprecated = deprecated
+        self._routes_version = 0
         # self._frontend_routes: _FrontendRouteGroup | None = None
         
+    # add new route into starlette.route
+    def add_api_route(
+        self,
+        path: str,
+        endpoint: Callable[..., Any], # Any kind of callable object
+        *,
+        methods: set[str] | list[str] | None = None,
+        name: str | None = None
+    ) -> None:
+        route = self.route_class(
+            self.prefix + path,
+            endpoint,
+            methods=methods,
+            name=name
+        )
+        self.routes.append(route)
+        self._mark_route_changed()
 
+    # Decorator
+    def api_route(
+        self,
+        path: str,
+        *,
+        methods: list[str] | None = None,
+        name: str | None = None
+    )-> Callable[[DecoratedCallable], DecoratedCallable]:
+        
+        def decorator(func: DecoratedCallable) -> DecoratedCallable:
+            self.add_api_route(
+                path,
+                func,
+                methods=methods,
+                name=name
+            )
+            return func
+        
+        return decorator
 
+    def get(
+        self,
+        path: Annotated[str, Doc("")],
+        name: Annotated[str | None, Doc("")] = None
+    )->Callable[[DecoratedCallable], DecoratedCallable]:
+        return self.api_route(
+            path,
+            name=name,
+            methods=["GET"]
+        )
+
+    def options(
+        self,
+        path: Annotated[str, Doc("")],
+        name: Annotated[str | None, Doc("")] = None
+    )->Callable[[DecoratedCallable], DecoratedCallable]:
+        return self.api_route(
+            path,
+            name=name,
+            methods=["OPTIONS"]
+        )
+
+    def head(
+        self,
+        path: Annotated[str, Doc("")],
+        name: Annotated[str | None, Doc("")] = None
+    )->Callable[[DecoratedCallable], DecoratedCallable]:
+        return self.api_route(
+            path,
+            name=name,
+            methods=["HEAD"]
+        )
+
+    def post(
+        self,
+        path: Annotated[str, Doc("")],
+        name: Annotated[str | None, Doc("")] = None
+    )->Callable[[DecoratedCallable], DecoratedCallable]:
+        return self.api_route(
+            path,
+            name=name,
+            methods=["POST"]
+        )
+
+    def put(
+        self,
+        path: Annotated[str, Doc("")],
+        name: Annotated[str | None, Doc("")] = None
+    )->Callable[[DecoratedCallable], DecoratedCallable]:
+        return self.api_route(
+            path,
+            name=name,
+            methods=["PUT"]
+        )
+
+    def delete(
+        self,
+        path: Annotated[str, Doc("")],
+        name: Annotated[str | None, Doc("")] = None
+    )->Callable[[DecoratedCallable], DecoratedCallable]:
+        return self.api_route(
+            path,
+            name=name,
+            methods=["DELETE"]
+        )
+
+    def patch(
+        self,
+        path: Annotated[str, Doc("")],
+        name: Annotated[str | None, Doc("")] = None
+    )->Callable[[DecoratedCallable], DecoratedCallable]:
+        return self.api_route(
+            path,
+            name=name,
+            methods=["PATCH"]
+        )
+
+    def trace(
+        self,
+        path: Annotated[str, Doc("")],
+        name: Annotated[str | None, Doc("")] = None
+    )->Callable[[DecoratedCallable], DecoratedCallable]:
+        return self.api_route(
+            path,
+            name=name,
+            methods=["TRACE"]
+        )
+
+    def _mark_route_changed(self) -> None:
+        self._routes_version += 1
